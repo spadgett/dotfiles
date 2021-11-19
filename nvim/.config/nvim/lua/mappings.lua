47522bbe2254481c09861d72a40b96ef60d8351c
Samuel Padgett <spadgett@redhat.com>
2021-11-19 07:52:41 -0500

    vim: Telescope buffers shortcut

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,3 +1,4 @@
 local map = vim.api.nvim_set_keymap
 
 map('n', '<c-p>', '<cmd>Telescope find_files find_command=fd,--type,f,--hidden<cr>', { noremap = true })
+map('n', 'gb', '<cmd>Telescope buffers<cr>', { noremap = true })
